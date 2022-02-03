4932bd97f433fc236677c9d2016e37e925e6ce08
elliotghr <gunsnroses.2617@gmail.com>
2022-02-02 18:29:27 -0600

    #3 Sintaxis JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let nombre = "Elliot",
+    estaciones = ["Primavera", "Verano", "Otoño", "Invierno"];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor="nombre">Nombre</label>
+        <input type="text" id="nombre" />
+        <p>{nombre}</p>
+        <p>{2 + 1}</p>
+        <p>
+          {/*A la inserción del DOM debe haber un identificador unico*/}
+          {estaciones.map((el, index) => (
+            <li key={index}>{el}</li>
+          ))}
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
